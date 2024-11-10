c4bcdbec5ca1ce37b44ba7d194dbfe79c026b9cf
Bruna0092 <50524997+Bruna0092@users.noreply.github.com>
2024-11-09 22:06:41 -0300

    Delete main directory

--- main/main.tfvars
+++
@@ -1,2 +0,0 @@
-cluster_name    = "cluster-karpenter"
-cluster_version = "1.30"
--- main/modules/vpc/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "private_subnets" {
-  value = {
-        "eks-public-us-east-1a" = aws_subnet.eks-public-us-east-1a.id
-        "eks-public-us-east-1b" = aws_subnet.eks-public-us-east-1b.id
-    }
-}
